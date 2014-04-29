2a621f0be5a404474e94a0da074c42f7805703af
Luka Muzinic <luka@muzinic.net>
2014-04-29 23:04:15 +0200

    Added Payload class

--- src/Slapper/Payload.php
+++ src/Slapper/Payload.php
@@ -5,5 +5,21 @@ namespace Slapper;
 
 class Payload
 {
+    public $channel;
+    public $username;
+    public $text;
+    public $icon_emoji;
 
-} 
+    public function __construct($text)
+    {
+        $this->text = $text;
+    }
+
+    public function prepare()
+    {
+        foreach($this as &$property)
+        {
+            $property = rawurlencode($property);
+        }
+    }
+}
--- tests/Slapper/PayloadTest.php
+++ tests/Slapper/PayloadTest.php
@@ -2,8 +2,36 @@
 
 class PayloadTest extends \PHPUnit_Framework_TestCase
 {
-    public function testExists()
+    protected $payload = null;
+
+    public function setUp()
     {
-        $payload = new Slapper\Payload();
+        $this->payload = new Slapper\Payload("Text that we send");
+    }
+
+    public function testProperties()
+    {
+        $this->assertObjectHasAttribute('channel', $this->payload);
+        $this->assertObjectHasAttribute('username', $this->payload);
+        $this->assertObjectHasAttribute('text', $this->payload);
+        $this->assertObjectHasAttribute('icon_emoji', $this->payload);
+    }
+
+    public function testPrepare()
+    {
+        $this->payload->channel = "#test";
+        $this->payload->icon_emoji = ":smile:";
+        $this->payload->text = '"}';
+
+        $this->payload->prepare();
+
+        $this->assertEquals('%23test', $this->payload->channel);
+        $this->assertEquals('%3Asmile%3A', $this->payload->icon_emoji);
+        $this->assertEquals('%22%7D', $this->payload->text);
+    }
+
+    public function tearDown()
+    {
+        unset($this->payload);
     }
 }
