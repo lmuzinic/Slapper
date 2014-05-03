a3e9f7888b347f32de38194411017420a85a0292
Luka Muzinic <luka@muzinic.net>
2014-05-04 00:22:34 +0200

    Added ValidateException
    Added validate method to Configuration that throws it.

--- src/Slapper/Configuration.php
+++ src/Slapper/Configuration.php
@@ -2,6 +2,7 @@
 
 namespace Slapper;
 
+use Slapper\Exception\ValidateException;
 
 class Configuration
 {
@@ -20,14 +21,34 @@ class Configuration
     }
 
     /**
+     * Checks if everything is as it should be
+     *
+     * @throws Exception\ValidateException
+     */
+    protected function validate()
+    {
+        if (is_null($this->token)) {
+            throw new ValidateException('Token must not be null');
+        }
+
+        if (is_null($this->teamUrl)) {
+            throw new ValidateException('Team URL must not be null');
+        }
+    }
+
+    /**
      * Writes configuration into file
+     *
+     * @return int number of bytes saved into configuration file
      */
     public function save()
     {
+        $this->validate();
+
         $lines = array();
         $lines[] = "token = " . $this->token;
         $lines[] = "teamUrl = " . $this->teamUrl;
 
-        file_put_contents($this->fileName, implode(PHP_EOL, $lines));
+        return file_put_contents($this->fileName, implode(PHP_EOL, $lines));
     }
 } 
---
+++ src/Slapper/Exception/ValidateException.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Slapper\Exception;
+
+
+class ValidateException extends \Exception
+{
+
+} 
